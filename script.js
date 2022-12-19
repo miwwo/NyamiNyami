79b6b0559efe0b68b78cec6ba21ca4b52cacd29b
miwwo <saifer.angel@yandex.ru>
2022-12-19 07:46:58 +0300

    commit

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 let min_price = document.querySelector('#min_price');
 let max_price = document.querySelector('#max_price');
 max_price.addEventListener('change', function () {
-    document.querySelector('#max_price').innerHTML = max_price.value;
-    let cards = document.querySelectorAll('.products');
-    cards.forEach(element => {
-        let elementPrice = element.querySelector('.cost_label').innerHTML.replace(/[^0-9]/g, "");
-        if (+elementPrice > +max_price.value) {
-            element.classList.add('hidden')
-        }
-        if (+elementPrice < +min_price.value) {
-            element.classList.remove('hidden')
-        }
-    });
+    let elementPrice = element.querySelector('.cost_label').innerHTML.replace(/[^0-9]/g, "");
+    if (+elementPrice > max_range.value || +elementPrice < min_range.value) {
+        element.classList.add('hidden')
+    }
+    if (+elementPrice < max_range.value && +elementPrice > min_range.value) {
+        element.classList.remove('hidden')
+    }
+})
+min_price.addEventListener('change', function () {
+    let elementPrice = element.querySelector('.cost_label').innerHTML.replace(/[^0-9]/g, "");
+    if (+elementPrice > max_range.value || +elementPrice < min_range.value) {
+        element.classList.add('hidden')
+    }
+    if (+elementPrice < max_range.value && +elementPrice > min_range.value) {
+        element.classList.remove('hidden')
+    }
 })
